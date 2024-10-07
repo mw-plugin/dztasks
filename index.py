f3d46b05bf05f51c29ff43bcea861062cb74a2c7
Mr Chen <midoks@163.com>
2024-10-07 13:48:16 +0800

    Update index.py

--- index.py
+++ index.py
@@ -260,74 +260,8 @@ def initdUinstall():
 
 
 def runLog():
-    return getServerDir() + '/data/logs.pl'
+    return getServerDir() + '/logs.pl'
 
-
-def getRedisConfInfo():
-    conf = getServerDir() + '/redis.conf'
-
-    gets = [
-        {'name': 'bind', 'type': 2, 'ps': '绑定IP(修改绑定IP可能会存在安全隐患)','must_show':1},
-        {'name': 'port', 'type': 2, 'ps': '绑定端口','must_show':1},
-        {'name': 'timeout', 'type': 2, 'ps': '空闲链接超时时间,0表示不断开','must_show':1},
-        {'name': 'maxclients', 'type': 2, 'ps': '最大连接数','must_show':1},
-        {'name': 'databases', 'type': 2, 'ps': '数据库数量','must_show':1},
-        {'name': 'requirepass', 'type': 2, 'ps': 'redis密码,留空代表没有设置密码','must_show':1},
-        {'name': 'maxmemory', 'type': 2, 'ps': 'MB,最大使用内存,0表示不限制','must_show':1},
-        {'name': 'slaveof', 'type': 2, 'ps': '同步主库地址','must_show':0},
-        {'name': 'masterauth', 'type': 2, 'ps': '同步主库密码', 'must_show':0}
-    ]
-    content = mw.readFile(conf)
-
-    result = []
-    for g in gets:
-        rep = r"^(" + g['name'] + r')\s*([.0-9A-Za-z_& ~]+)'
-        tmp = re.search(rep, content, re.M)
-        if not tmp:
-            if g['must_show'] == 0:
-                continue
-
-            g['value'] = ''
-            result.append(g)
-            continue
-        g['value'] = tmp.groups()[1]
-        if g['name'] == 'maxmemory':
-            g['value'] = g['value'].strip("mb")
-        result.append(g)
-
-    return result
-
-
-def getRedisConf():
-    data = getRedisConfInfo()
-    return mw.getJson(data)
-
-
-def submitRedisConf():
-    gets = ['bind', 'port', 'timeout', 'maxclients',
-            'databases', 'requirepass', 'maxmemory','slaveof','masterauth']
-    args = getArgs()
-    conf = getServerDir() + '/redis.conf'
-    content = mw.readFile(conf)
-    for g in gets:
-        if g in args:
-            rep = g + r'\s*([.0-9A-Za-z_& ~]+)'
-            val = g + ' ' + args[g]
-
-            if g == 'maxmemory':
-                val = g + ' ' + args[g] + "mb"
-
-            if g == 'requirepass' and args[g] == '':
-                content = re.sub('requirepass', '#requirepass', content)
-            if g == 'requirepass' and args[g] != '':
-                content = re.sub('#requirepass', 'requirepass', content)
-                content = re.sub(rep, val, content)
-
-            if g != 'requirepass':
-                content = re.sub(rep, val, content)
-    mw.writeFile(conf, content)
-    reload()
-    return mw.returnJson(True, '设置成功')
 
 if __name__ == "__main__":
     func = sys.argv[1]
